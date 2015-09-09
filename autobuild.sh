20eee6ec0be99b0711764a4128a9d96b127e5ff5
snailwalker <qiyu1001@gmail.com>
2015-09-09 14:23:44 +0800

    Update autobuild.sh

--- autobuild.sh
+++ autobuild.sh
@@ -2,15 +2,21 @@
 DATE=$(date +%Y%m%d%H%M)
 WAR_NAME=###
 PATH_NAME=###
-StartTomcat=/usr/local/tomcat7/bin/startup.sh
+Jenkins_Job_Name=/home/jenkins/workspace/pe-$PATH_NAME
 TomcatCache=/usr/local/tomcat7/work
 echo =============================备份war包======================
-cp /var/www/webapps/$WAR_NAME.war /data/war_bak/$PATH_NAME.war_$DATE
+cp $Jenkins_Job_Name/$WAR_NAME-web/target/$PATH_NAME.war /data/war_bak/$PATH_NAME.war_$DATE
 echo =============================开始重新部署======================
-rm -rf /usr/local/tomcat7/webapps/$PATH_NAME
-mv  PATH-TO-WAR/$WAR_NAME.war /var/www/webapps/$PATH_NAME.war
-ps aux|grep /usr/local/tomcat7/bin/|grep -v grep |awk '{print $2}'|xargs kill
-rm -rf $TomcatCache
-sleep 5
-$StartTomcat
-echo =============================部署完毕======================
+mv /home/jenkins/workspace/pe-$PATH_NAME/$WAR_NAME-web/target/$PATH_NAME.war /var/www/webapps/$PATH_NAME.war
+PID=`ps aux|grep /data/tomcat7/bin/|grep -v grep |awk '{print $2}'`
+if [ -n "$PID" ]; then
+    echo 原tomcat进程ID: $PID
+    kill $PID
+    rm -rf /data/tomcat7/webapps/$PATH_NAME
+    rm -rf $TomcatCache
+fi
+sleep 10
+sh /data/tomcat7/bin/startup.sh
+PID=`ps aux|grep /data/tomcat7/bin/|grep -v grep |awk '{print $2}'`
+echo 当前tomcat进程ID: $PID
+echo  =============================部署完毕======================
